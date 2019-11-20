6408882b103c06f7b48590076102571d6eb5f027
AlissonSteffens <ali.steffens@gmail.com>
2019-11-20 20:25:22 -0300

    :dizzy: animating on loading table

--- src/views/Frota.jsx
+++ src/views/Frota.jsx
@@ -3,40 +3,78 @@ import { Grid, Row, Col, Table } from "react-bootstrap";
 
 import Card from "components/Card/Card.jsx";
 import { thArray, tdArray } from "variables/Variables.jsx";
+import Skeleton from 'react-loading-skeleton';
 
 class Frota extends Component {
+  state = {
+    isLoading: true,
+    buses: [],
+    error: null
+  }
+  fetchUsers() {
+    // Where we're fetching data from
+    fetch(`https://cors-anywhere.herokuapp.com/https://opksmartbusao.herokuapp.com/api/buses`)
+      // We get the API response and receive data in JSON format...
+      .then(response => response.json())
+      // ...then we update the users state
+      .then(data =>
+        this.setState({
+          buses: data,
+          isLoading: false,
+        })
+      )
+      // Catch any errors we hit and update the app
+      .catch(error => this.setState({ error, isLoading: false }));
+  }
+  componentDidMount() {
+    this.fetchUsers();
+  }
   render() {
+    const { isLoading, buses, error } = this.state;
     return (
       <div className="content">
         <Grid fluid>
           <Row>
             <Col md={12}>
               <Card
-                title="Striped Table with Hover"
-                category="Here is a subtitle for this table"
+                title="Frota da Empresa"
+                category="Lista com todos os ônibus da empresa"
                 ctTableFullWidth
                 ctTableResponsive
                 content={
+                  
                   <Table striped hover>
                     <thead>
                       <tr>
-                        {thArray.map((prop, key) => {
-                          return <th key={key}>{prop}</th>;
-                        })}
+                        <th>Número</th>
+                        <th>Assentos</th>
+                        <th>Capacidade</th>
+                        <th>Chassi</th>
+                        <th>Lotação</th>
+                        <th>Ações</th>
                       </tr>
                     </thead>
                     <tbody>
-                      {tdArray.map((prop, key) => {
-                        return (
-                          <tr key={key}>
-                            {prop.map((prop, key) => {
-                              return <td key={key}>{prop}</td>;
-                            })}
-                          </tr>
-                        );
-                      })}
+                      {!isLoading ? (
+                        buses.map(bus => {
+                          return (
+                            <tr key={bus.number}>
+                              <td>{bus.number}</td>
+                              <td>{bus.seats}</td>
+                              <td>{bus.totalCapacity}</td>
+                              <td>{bus.chassi}</td>
+                              <td>{bus.passengersNum}</td>
+                              <td style={{textAlign: "center"}}><a href="#"><i className="pe-7s-map-marker" /></a><a href="#"><i className="pe-7s-pen" /></a></td>
+                            </tr>
+                          );
+                        })
+                      // If there is a delay in data, let's let the user know it's loading
+                      ) : (
+                        <tr ><Skeleton width={100} count={10}/></tr>
+                      )}
                     </tbody>
                   </Table>
+                  
                 }
               />
             </Col>
